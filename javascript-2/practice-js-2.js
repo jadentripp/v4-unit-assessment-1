88c4d0c78d1a9dd84117d42fc4ef807d4ff730b9
yourusername <jadenitripp@gmail.com>
2021-04-09 12:51:01 -0600

    added definitions

--- javascript-2/practice-js-2.js
+++ javascript-2/practice-js-2.js
@@ -10,7 +10,7 @@
 */
 
 //CODE HERE
-
+let gitDefinition = 'Git is a tool used by developers within the command line interface which is used to save changes made to files, commit them, and then push the files to a repository on github.'
 //////////////////PROBLEM 2////////////////////
 /*
     Create a variable called 'gitHubDefinition'.  
@@ -18,7 +18,7 @@
 */
 
 //CODE HERE
-
+let gitHubDefinition = 'Github is a way for developers to keep track of changes made to their files. Github is especially useful for tracking changes made to large repositories along with allowing other people to view a persons published code and make changes without editing the master.'
 //////////////////PROBLEMS 3 - 9////////////////////
 /*
     For the next several problems you will be creating objects containing information about different git 
@@ -34,7 +34,10 @@
 */
 
 //CODE HERE
-
+let init = {
+    description:'Git init will initialize a repository for you. This command does not need to be performed on cloned repositories.',
+    code: 'git init repository.name'
+}
 //////////////////PROBLEM 4////////////////////
 /*
     Create an object called 'clone' with 'description' and 'code' properties 
@@ -42,7 +45,10 @@
 */
 
 //CODE HERE
-
+let clone = {
+    description:'Git clone is useful for when you have forked a repository from github. Its name is self explanatory. It clones a repository so that you can make edits without changing another persons code. After forking the repository, simply copy the url for the repository you are cloning, use terminal or whichever CLI you use to cd to your directory you would like the repository to be cloned to, and type',
+    code: 'git clone url'
+}
 //////////////////PROBLEM 5////////////////////
 /*
     Create an object called 'status' with 'description' and 'code' properties 
@@ -50,7 +56,10 @@
 */
 
 //CODE HERE
-
+let status = {
+    description: 'Status is a useful tool for seeing the status of files within you present working directory. Status will show you whether or not a file has been saved or committed. If you run this command and see that files are red, use git add . to tell git to save them.',
+    code: 'git status'
+}
 //////////////////PROBLEM 6////////////////////
 /*
     Create an object called 'add' with 'description' and 'code' properties 
@@ -58,7 +67,10 @@
 */
 
 //CODE HERE
-
+let add = {
+    description: 'Add is a command that will tell git to start saving the files that are in your present working directory.',
+    code: 'git add .'
+}
 //////////////////PROBLEM 7////////////////////
 /*
     Create an object called 'commit' with 'description' and 'code' properties
@@ -66,7 +78,10 @@
 */
 
 //CODE HERE
-
+let commit = {
+    description:'commit will stage your changes to github and commit them. This is the final step before pushing your files to github.' ,
+    code:'git commit -m "comment about your code and changes made to it"'
+}
 //////////////////PROBLEM 8////////////////////
 /*
     Create an object called 'addRemote' with 'description' and 'code' properties
@@ -74,7 +89,10 @@
 */
 
 //CODE HERE
-
+let addRemote = {
+    description:'The git remote command lets you create, view, and delete connections to other repositories. Remote connections are more like bookmarks rather than direct links into other repositories. Instead of providing real-time access to another repository, they serve as convenient names that can be used to reference a not-so-convenient URL.' ,
+    code:'git remote'
+}
 //////////////////PROBLEM 9////////////////////
 /*
     Create an object called 'push' with 'description' and 'code' properties 
@@ -82,3 +100,7 @@
 */
 
 //CODE HERE
+let push = {
+    description:'The git push command is used to upload local repository content to a remote repository. Pushing is how you transfer commits from your local repository to a remote repo.' ,
+    code:'git push master origin'
+}
